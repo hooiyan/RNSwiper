105679a8d05242cefedac80cf66671e23bd50e4c
hooiyan <hoecoding@gmail.com>
2022-06-19 20:58:39 +0800

    Carousel is working now

--- App.js
+++ App.js
@@ -6,74 +6,92 @@
  * @flow strict-local
  */
 
-import React, {useState, useCallback, useRef} from 'react';
-import {Text, View, SafeAreaView} from 'react-native';
-import Carousel from 'react-native-snap-carousel';
-
-const exampleItems = [
-  {
-    title: 'Item 1',
-    text: 'Text 1',
-  },
-  {
-    title: 'Item 2',
-    text: 'Text 2',
-  },
-  {
-    title: 'Item 3',
-    text: 'Text 3',
-  },
-  {
-    title: 'Item 4',
-    text: 'Text 4',
-  },
-  {
-    title: 'Item 5',
-    text: 'Text 5',
-  },
-];
+import axios from 'axios';
+import React, {useState, useCallback, useRef, useEffect} from 'react';
+import {Text, View, SafeAreaView, Dimensions, Button} from 'react-native';
+import Carousel from 'react-native-reanimated-carousel';
 
 const App = () => {
+  const PAGE_WIDTH = Dimensions.get('window').width;
   const [activeIndex, setActiveIndex] = useState(0);
-  const [carouselItems, setCarouselItems] = useState(exampleItems);
+  const [data, setData] = useState([...new Array(6).keys()]);
+  const [isVertical, setIsVertical] = useState(false);
+  const [isFast, setIsFast] = useState(false);
+  const [isAutoPlay, setIsAutoPlay] = useState(false);
   const ref = useRef(null);
+  const [result, setResults] = useState([]);
 
-  const renderItem = useCallback(
-    ({item, index}) => (
-      <View
-        style={{
-          backgroundColor: 'floralwhite',
-          borderRadius: 5,
-          height: 250,
-          padding: 50,
-          marginLeft: 25,
-          marginRight: 25,
-        }}>
-        <Text style={{fontSize: 30}}>{item.title}</Text>
-        <Text>{item.text}</Text>
-      </View>
-    ),
-    [],
-  );
+  useEffect(() => {
+    fetchData();
+    console.log(result);
+  }, []);
+
+  const fetchData = (date = '') => {
+    const API = 'https://api.4dnum.com/api/v1/result';
+    axios
+      .get(`${API}/${date}`)
+      .then(res => res.data !== undefined && setResults(res.data))
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  const baseOptions = isVertical
+    ? {
+        vertical: true,
+        width: PAGE_WIDTH,
+        height: PAGE_WIDTH,
+      }
+    : {
+        vertical: false,
+        width: PAGE_WIDTH,
+        height: PAGE_WIDTH,
+      };
+
+  // console.log(result);
 
   return (
-    <SafeAreaView
-      style={{flex: 1, backgroundColor: 'rebeccapurple', paddingTop: 50}}>
-      <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
+    <SafeAreaView style={{flex: 1}}>
+      <View
+        style={{
+          flex: 1,
+          flexGrow: 1,
+          // display: 'flex',
+          // backgroundColor: 'rebeccapurple',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Text style={{color: 'black'}}>Hello There</Text>
         <Carousel
+          {...baseOptions}
+          defaultIndex={0}
           loop
-          layout="default"
-          layoutCardOffset={`0`}
           ref={ref}
-          data={carouselItems}
-          sliderWidth={300}
-          itemWidth={300}
-          renderItem={renderItem}
+          autoPlay={isAutoPlay}
+          autoPlayInterval={isFast ? 100 : 2000}
+          data={result}
           onSnapToItem={index => {
-            console.log(index);
+            console.log('current index:', index);
             setActiveIndex(index);
           }}
+          renderItem={({index}) => {
+            return (
+              <Text
+                key={index}
+                index={index}
+                style={{
+                  fontSize: 30,
+                  backgroundColor: 'pink',
+                  color: 'black',
+                  textAlign: 'center',
+                }}>
+                {index}
+                {/* {result.find(i => i.type === 'GD').fdData.s1} */}
+              </Text>
+            );
+          }}
         />
+        <Button title="Click me" onPress={() => fetchData('2022-06-01')} />
       </View>
     </SafeAreaView>
   );
